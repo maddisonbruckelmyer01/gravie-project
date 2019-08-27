641314054b45774bef91471cb3533851a0b48a30
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-08-27 15:56:00 -0500

    base goals done

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/', (req, res) => {
-    res.send('Hello World'); // Replace this
+    axios.get(`http://api.giphy.com/v1/gifs/random?key=${process.env.GIPHY_API_KEY}`)
+    .then(response=>{
+        console.log(response.data.data.images.downsized_large.url)
+        res.send(response.data.data.images.downsized_large.url)
+    }).catch(error => {
+        console.log(error)
+    })
 })
 
 module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import {connect} from 'react-redux';
 
 class App extends Component {
   // Renders the entire app on the DOM
+  componentDidMount = () =>{
+    this.getGiphy();
+  }
+
+  getGiphy = () => {
+    axios.get('/random').then(response => {
+      console.log(response)
+      this.props.dispatch({
+        type: 'SET_RANDOM',
+        payload: response.data
+      })
+    }).catch(error => {
+      console.log(error)
+    })
+  }
   render() {
     return (
       <div>
         <header className="App-header">
           <h1>Random Giphy API</h1>
         </header>
-        
         <p>Results go here</p>
+        <img src={this.props.storeInstance.random} />
+        <button onClick={this.componentDidMount}>New Image</button>
+   
       </div>
     );
   }
 }
+const mapStateToProps = (storeInstance) => {
+  return {
+    storeInstance
+  }
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
